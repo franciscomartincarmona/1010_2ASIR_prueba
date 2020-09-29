413235f2840a0d4def72486f0e7da9bf958b772d
Francisco Martin Carmona <fmartincarmona19@iespuntadelverde.es>
2020-09-29 19:28:57 +0200

    first commit

---
+++ src/tiposbasicos.ts
@@ -0,0 +1,11 @@
+let activo: boolean;
+let valor: number
+let cantidad: any
+let nombre: string
+let lista: number[]
+activo = true
+if (activo){
+    console.log('Está activo')
+} else {
+    console.log('No está activo')
+}
