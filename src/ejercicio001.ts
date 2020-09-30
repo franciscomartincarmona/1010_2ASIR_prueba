fd2da0a723a7a1987e67e04e6b938b7a80072527
martincarmonafrancisco <72010665+martincarmonafrancisco@users.noreply.github.com>
2020-09-30 08:31:23 +0200

    Update ejercicio001.ts

--- src/ejercicio001.ts
+++ src/ejercicio001.ts
@@ -1,19 +1,1 @@
-console.log('Hola mundo')
 
-function sumar (a: number, b: number) {
-    return a + b
-}
-
-console.log(sumar(3, 4))
-
-let restar = (a: number, b: number) => a - b
-console.log(restar(3, 4))
-
-let multiplicaRaro = (a: number, b: number): number => {
-    let r: number
-    let h: number
-    r = 4
-    let r2 = a * b + r
-    return r2
-}
-console.log(multiplicaRaro(3, 4) + 5)
