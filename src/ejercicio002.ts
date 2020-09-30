9e62169a773872b82e891a1f4125bac7575d6eda
Francisco Martin Carmona <fmartincarmona19@iespuntadelverde.es>
2020-09-30 09:36:35 +0200

    first commit

--- src/ejercicio002.ts
+++ src/ejercicio002.ts
@@ -1,7 +1,7 @@
 let nombreP:string = "Rafa Nadal";
 let edad:number   = 34;
 const PERSONAJE: { nombre:string, edad:number } = {
-    nombre: nombre,
+    nombre: nombreP,
     edad: edad
 }
 console.log(PERSONAJE);
