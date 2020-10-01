4d979f31f971ae4a5ad91b5d4744b641f6332da4
Francisco Martin Carmona <fmartincarmona19@iespuntadelverde.es>
2020-10-01 20:06:12 +0200

    first commit

--- src/ejercicio002.ts
+++ src/ejercicio002.ts
@@ -1,3 +1,7 @@
+/*he creado dos variables con el nombre y edad y 
+le he asignado los datos de un personaje, 
+se han indicado los tipos de datos, 
+en este caso string para nombre y number para edad.*/
 let nombreP:string = "Rafa Nadal";
 let edad:number = 34;
 const PERSONAJE: { nombre:string, edad:number } = {
--- src/ejercicio003.ts
+++ src/ejercicio003.ts
@@ -1,3 +1,7 @@
+/*he creado una variable para que me sume dos números y 
+al sumarlos se multiplique por 2 y 
+así sea el resultado doble de esa suma.*/
+
 let resultDoble = (a:number , b:number) => (a + b )* 2;
 
 console.log(resultDoble(5,5));
